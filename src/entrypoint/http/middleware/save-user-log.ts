965dfac6b22edbe8d6e2246d38f0aa17c4caeb5d
Mark Vayngrib <mvayngrib@gmail.com>
2019-02-09 17:37:50 -0500

    style

--- src/entrypoint/http/middleware/save-user-log.ts
+++ src/entrypoint/http/middleware/save-user-log.ts
@@ -4,18 +4,18 @@ import pick = require('lodash/pick')
 export const create = (): Middleware => async (ctx, next) => {
   let opts: PutUserLogOpts
 
-  const { headers, request, query } = ctx
+  const { container, headers, request, query } = ctx
   const { body } = request
   // if (headers['content-type'] === 'text') {
   //   opts = {
   //     firstName: query.firstName,
   //     lastName: query.lastName,
-  //     log: body
+  //     log: rawBody
   //   }
   // } else {
-  opts = pick(ctx.request.body, ['firstName', 'lastName', 'log']) as PutUserLogOpts
+  opts = pick(body, ['firstName', 'lastName', 'log']) as PutUserLogOpts
   // }
 
-  await ctx.container.userLogs.put(opts)
+  await container.userLogs.put(opts)
   await next()
 }
